a1c7a166eb0f0697b593d8633348e5a3f5bdd3c8
Dalibor Ocenasek <257032@vutbr.cz>
2024-04-08 12:20:03 +0200

    message

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -13,9 +14,22 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    with open (file_name, "r") as file:
+        my_dict = json.load(file)
+    my_keys = my_dict.keys()
+
+    if field not in my_keys:
+        return None
+    else:
+        return my_dict[field]
+
+
 
 def main():
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
     pass
+
 
 
 if __name__ == '__main__':
